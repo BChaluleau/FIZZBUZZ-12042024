473ef0a53c478afa5d0ab9cdb681606243b658aa
bch <bollombollom@yahoo.fr>
2024-04-12 13:53:06 +0200

    ITER1 : fizz(1)

--- src/fizzbuzz/FizzBuzz.java
+++ src/fizzbuzz/FizzBuzz.java
@@ -2,4 +2,8 @@ package fizzbuzz;
 
 public class FizzBuzz {
 
+	public String game(int i) {
+		return "1";
+	}
+
 }
--- test/fizzbuzz/FizzBuzzTest.java
+++ test/fizzbuzz/FizzBuzzTest.java
@@ -1,16 +1,22 @@
 package fizzbuzz;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class FizzBuzzTest {
 
+	FizzBuzz fb;
+
 	@BeforeEach
 	void setUp() throws Exception {
+		fb = new FizzBuzz();
 	}
 
 	@Test
-	void test() {
+	void fizzBuzzUn() {
+		assertEquals("1", fb.game(1));
 	}
 
 }
